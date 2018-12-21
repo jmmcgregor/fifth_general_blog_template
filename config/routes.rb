951c20a37122081fd9c0540fb1953a16ae6a4584
Unknown <bmcgregor@fifthpartners.com>
2018-12-21 12:54:42 -0700

    generated a Sessions controller for logging in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root 'blog_posts#index'
   resources :blog_users
   resources :blog_posts
   resources :blog_photos
-  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
