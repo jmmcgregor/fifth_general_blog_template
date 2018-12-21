1e5298005b81fbc7ed6adb0ab5a300fc8c53fcf0
Unknown <bmcgregor@fifthpartners.com>
2018-12-21 14:31:55 -0700

    Log in functional and users not logged in are unable to access new blog

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get '/new', to: 'blog_posts#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
